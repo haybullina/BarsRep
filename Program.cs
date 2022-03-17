942649173b342e5558a5395abf2a1ff9ded7c80a
haybullina <101605765+haybullina@users.noreply.github.com>
2022-03-17 20:01:35 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace bars
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello Bars!");
+        }
+    }
+}
