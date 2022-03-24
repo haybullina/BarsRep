277260793495e4a2987949ab29c00f8e2167d583
haybullina <101605765+haybullina@users.noreply.github.com>
2022-03-24 11:06:46 +0300

    Add files via upload

---
+++ KeyboardSubscriber.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+
+namespace BarsRep
+{
+    public class KeyboardSubscriber
+    {
+        private Keyboard kb = new Keyboard();
+
+        public KeyboardSubscriber()
+        {
+            kb.OnKeyPressed += PrintChar;
+            
+            kb.Run();
+        }
+
+        private void PrintChar(Object sender, char ch)
+        {
+            Console.WriteLine($"\n{ch}");
+        }
+    } 
+}
