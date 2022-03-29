ba0af6fc987052f7b53338ad47ae430e50c79f71
haybullina <101605765+haybullina@users.noreply.github.com>
2022-03-29 19:17:21 +0300

    Add files via upload

---
+++ Entity/Entity.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+
+namespace Entity
+{
+    public class Entity
+    {
+        public int Id;
+        public int ParentId;
+        public string Name;
+    }
+}
---
+++ Entity/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Entity
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Entity> list = new List<Entity>()
+            {
+                new Entity { Id = 1, ParentId = 0, Name = "Root entity"},
+
+                new Entity { Id = 2, ParentId = 1, Name = "Child of 1 entity"},
+
+                new Entity { Id = 3, ParentId = 1, Name = "Child of 1 entity"},
+
+                new Entity { Id = 4, ParentId = 2, Name = "Child of 2 entity"},
+
+                new Entity { Id = 5, ParentId = 4, Name = "Child of 4 entity"}
+            };
+            var a = ArrangeParents(list);
+            Console.WriteLine(a);
+        }
+
+        public static Dictionary<int, List<Entity>> 
+            ArrangeParents(List<Entity> entities)
+        {
+            var result = new Dictionary<int, List<Entity>>();
+            foreach (var entity in entities)
+            {
+                result.TryAdd(entity.ParentId, new List<Entity>());
+                result[entity.ParentId].Add(entity);
+            }
+
+            return result;
+        }
+    }
+}
