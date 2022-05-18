ae5789ad571c2e9a23649795a3bb574e4d2aec1c
haybullina <101605765+haybullina@users.noreply.github.com>
2022-05-18 23:41:12 +0300

    Add files via upload

--- CSV_Parsing — копия/Program.cs
+++ CSV_Parsing — копия/Program.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Diagnostics;
 using System.IO;
+using System.Threading;
+using System.Xml.Linq;
 
 namespace CSV_Parsing
 {
@@ -9,14 +11,19 @@ namespace CSV_Parsing
         static void Main(string[] args)
         {
             string[] arrSplit = new string[] {"=", ";"};
-            Stopwatch stopWatch = new Stopwatch();
+            Stopwatch stopwatch = new Stopwatch();
             string path = @"text";
             string newPath;
 
             if (Directory.Exists(path))
             {
                 string[] filesArr = Directory.GetFiles(path);
-                
+                foreach (var value in filesArr)
+                {
+                    newPath = Console.ReadLine();
+                    ThreadPool.QueueUserWorkItem(callBack => ParsingText(value, newPath));
+                }
+                /*
                 foreach (var value in filesArr)
                 {
                     newPath = Console.ReadLine();
@@ -34,17 +41,45 @@ namespace CSV_Parsing
                                 string[] arrayTemp = temp.Split(arrSplit, StringSplitOptions.RemoveEmptyEntries);
                                 if (temp.Split()[0] == "Play")
                                 {
-                                    newFile.Write($"{arrayTemp[2]}, {arrayTemp[4]}, ");
                                     string dateTime = (DateTime.Parse(arrayTemp[6]) - DateTime.Parse(arrayTemp[4]))
                                         .Seconds.ToString();
-                                    newFile.Write($"{dateTime}");
+                                    newFile.Write($"{arrayTemp[2]}, {arrayTemp[4]}, {dateTime}");
                                     newFile.WriteLine();
                                 }
                             }
                         }
                         stopWatch.Stop();
+                        
+                    }*/
+                    stopwatch.Stop();
+                    Console.WriteLine(stopwatch.Elapsed);
+            }
+
+            static void ParsingText(string path, string newPath)
+            {
+                string[] arrSplit = new string[] {"=", ";"};
+                Stopwatch stopWatch = new Stopwatch();
+
+                using (StreamWriter newFile = new StreamWriter($"{newPath}.txt"))
+                {
+                    using (StreamReader file = new StreamReader(path))
+                    {
+                        while (true)
+                        {
+                            string temp = file.ReadLine();
+
+                            if (temp == null) break;
+
+                            string[] arrayTemp = temp.Split(arrSplit, StringSplitOptions.RemoveEmptyEntries);
+                            if (temp.Split()[0] == "Play")
+                            {
+                                string dateTime = (DateTime.Parse(arrayTemp[6]) - DateTime.Parse(arrayTemp[4]))
+                                    .Seconds.ToString();
+                                newFile.Write($"{arrayTemp[2]}, {arrayTemp[4]}, {dateTime}");
+                                newFile.WriteLine();
+                            }
+                        }
                     }
-                    Console.WriteLine(stopWatch.Elapsed);
                 }
             }
         }
