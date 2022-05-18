e4a18a22325838f275f947cdcad1ffe4e137953e
haybullina <101605765+haybullina@users.noreply.github.com>
2022-05-19 02:04:59 +0300

    Add files via upload

--- CSV_Parsing — копия/Program.cs
+++ CSV_Parsing — копия/Program.cs
@@ -2,7 +2,7 @@
 using System.Diagnostics;
 using System.IO;
 using System.Threading;
-using System.Xml.Linq;
+using System.Threading.Tasks;
 
 namespace CSV_Parsing
 {
@@ -13,54 +13,36 @@ namespace CSV_Parsing
             string[] arrSplit = new string[] {"=", ";"};
             Stopwatch stopwatch = new Stopwatch();
             string path = @"text";
-            string newPath;
+            string[] newName = new string []{"1","2","3","4","5",};
+            Task[] tasks;
 
             if (Directory.Exists(path))
             {
                 string[] filesArr = Directory.GetFiles(path);
-                foreach (var value in filesArr)
+                
+                tasks = new Task[filesArr.Length];
+                stopwatch.Start();
+
+                for (var i = 0; i < tasks.Length; ++i)
                 {
-                    newPath = Console.ReadLine();
-                    ThreadPool.QueueUserWorkItem(callBack => ParsingText(value, newPath));
-                }
-                /*
-                foreach (var value in filesArr)
-                {
-                    newPath = Console.ReadLine();
-                    using (StreamWriter newFile = new StreamWriter($"{newPath}.txt"))
+                    tasks[i] = Task.Run(() =>
                     {
-                        stopWatch.Start();
-                        using (StreamReader file = new StreamReader(value))
+                        if (i < tasks.Length)
                         {
-                            while (true)
-                            {
-                                string temp = file.ReadLine();
-
-                                if (temp == null) break;
-
-                                string[] arrayTemp = temp.Split(arrSplit, StringSplitOptions.RemoveEmptyEntries);
-                                if (temp.Split()[0] == "Play")
-                                {
-                                    string dateTime = (DateTime.Parse(arrayTemp[6]) - DateTime.Parse(arrayTemp[4]))
-                                        .Seconds.ToString();
-                                    newFile.Write($"{arrayTemp[2]}, {arrayTemp[4]}, {dateTime}");
-                                    newFile.WriteLine();
-                                }
-                            }
+                            ParsingText(filesArr[i], newName[i]);
                         }
-                        stopWatch.Stop();
-                        
-                    }*/
-                    stopwatch.Stop();
-                    Console.WriteLine(stopwatch.Elapsed);
+                    });
+                }
+                Task.WaitAll(tasks);
+                stopwatch.Stop();
+                Console.WriteLine(stopwatch.Elapsed);
             }
 
-            static void ParsingText(string path, string newPath)
+            static void ParsingText(string path, string newName)
             {
                 string[] arrSplit = new string[] {"=", ";"};
-                Stopwatch stopWatch = new Stopwatch();
 
-                using (StreamWriter newFile = new StreamWriter($"{newPath}.txt"))
+                using (StreamWriter newFile = new StreamWriter($"{newName}.txt"))
                 {
                     using (StreamReader file = new StreamReader(path))
                     {
